c881f97999904c2f1cbd191e5e29049c3ccba65a
muhammadrashwan1101 <muhammadrashwan1101@gmail.com>
2026-03-28 11:48:44 +0200

    Added Extra Commands

--- LinearEquationCalc/LinearEquationCalc.cpp
+++ LinearEquationCalc/LinearEquationCalc.cpp
@@ -2,15 +2,17 @@
 #include <string>
 #include <limits>
 #include <cctype>
+#include <sstream>
 
 using namespace std;
 
 int main()
 {
     string equations[100];
-    float coefficient[100][100], xIndex[100], RHS_Arr[100];
+    string org_equations[100];
+    float coefficient[100][100], RHS_Arr[100];
     int n;
-    string command;
+
 
     //Insert the number of equations
     cin >> n;
@@ -107,7 +109,8 @@ int main()
                 float value = stof(terms[termIndex]);
                 if (!onRightSide) {
                     LHS_const += value;
-                } else {
+                }
+                else {
                     RHS_Arr[i] += value;
                 }
                 continue;
@@ -177,29 +180,369 @@ int main()
     // Running Commnads
     // ================================
     bool quit = true;
+    string command;
+
     while (quit) {
-        getline(cin,command);
+        getline(cin, command);
+
         string eqCheck = command.substr(0, 8);
-
-
-        if (command == "num_vars"){
+        if (command == "num_vars") {
             cout << "Number of Values = " << n << "\n";
         }
 
+
+        // ================================
+        // PRINT EQUATION OF N
+        // ================================
         if (eqCheck == "equation") {
-            int eNumber = 0;
-            for (int i = command.length() - 1; i >= 0; --i) {
-                if (isdigit(command[i])) {
-                    eNumber = eNumber * 10 + (command[i] - '0');
-                    if (eNumber > n) {
-                        cout << "No Equations listed with that index, Last available equation = " << n << endl;
+            int eNumber = stoi(command.substr(9)); // number after "equation "
+            //Check if there's no equation with this index
+            if (eNumber <= 0 || eNumber > n) {
+                cout << "No Equations listed with that index, Last available equation = " << n << endl;
+                continue;
+            }
+            int row = eNumber - 1;
+            cout << "Equation " << eNumber << ": ";
+            //Needed to recognize if it's the first term so we can ignore any operators at the beginning of the term
+            bool firstTerm = true;
+            for (int col = 0; col < n; col++) {
+                float coeff = coefficient[row][col];
+                if (coeff == 0)
+                    continue;
+                //If it's not the first term anymore and the coeff is positive We are double checking that the sign is a plus 
+                if (!firstTerm && coeff > 0) {
+                    cout << "+";
+                }
+                cout << coeff << "x" << col + 1;
+                //Disable the firsTerm flag so the operators at the beginning of each term to be further used normally 
+                firstTerm = false;
+            }
+            cout << " = " << RHS_Arr[row] << endl;
+        }
+
+
+        if (command.substr(0, 6) == "column") {
+            int index = stoi(command.substr(8));
+            for (int i = 0; i < n; i++) {
+                if (index <= 0 || index > n) {
+                    cout << "No Columns listed under that index, Last available column of variables = " << n << endl;
+                    continue;
+                }
+                cout << coefficient[i][index - 1] << endl;
+            }
+        }
+
+        // ================================
+        // ADD 2 EQUATIONS
+        // ================================
+        if (command.substr(0, 3) == "add") {
+            //Get the first equation number and the second
+            string word;
+            int eq1, eq2;
+            stringstream ss(command);
+            ss >> word;
+            ss >> eq1;
+            ss >> eq2;
+
+            //Check if the equations number is valid
+            if (eq1 <= 0 || eq2 <= 0 || eq1 > n || eq2 > n) {
+                cout << "No equations available with this index, equations start from 1 to " << n << endl;
+                continue;
+            }
+
+            //Adjust the equation numbers to be used in the coefficients array
+            int row1 = eq1 - 1;
+            int row2 = eq2 - 1;
+
+            float RHSSum;
+            bool firstTerm = true;
+            for (int col = 0; col < n; col++) {
+                // Add Coefficients and RHS values 
+                float coeff1 = coefficient[row1][col];
+                float coeff2 = coefficient[row2][col];
+                float coeffSum = coeff1 + coeff2;
+
+                float RHS1 = RHS_Arr[row1];
+                float RHS2 = RHS_Arr[row2];
+                RHSSum = RHS1 + RHS2;
+
+                //Skip any term that will result a 0 Coefficient
+                if (coeffSum == 0)
+                    continue;
+                if (!firstTerm && coeffSum > 0) {
+                    cout << "+";
+                }
+                //print the output term
+                cout << coeffSum << "x" << col + 1;
+                firstTerm = false;
+            }
+            //print the RHS
+            cout << " = " << RHSSum << endl;
+        }
+
+        // ================================
+        // SUBTRACT 2 EQUATIONS
+        // ================================
+        if (command.substr(0, 8) == "subtract") {
+            //Get the first equation number and the second
+            string word;
+            int eq1, eq2;
+            stringstream sepCommand(command);
+            sepCommand >> word;
+            sepCommand >> eq1;
+            sepCommand >> eq2;
+
+            if (eq1 <= 0 || eq2 <= 0 || eq1 > n || eq2 > n) {
+                cout << "No equations available with this index, equations start from 1 to " << n << endl;
+                continue;
+            }
+
+            int row1 = eq1 - 1;
+            int row2 = eq2 - 1;
+
+            float RHSSum;
+            bool firstTerm = true;
+            for (int col = 0; col < n; col++) {
+                float coeff1 = coefficient[row1][col];
+                float coeff2 = coefficient[row2][col];
+                float coeffSum = coeff1 - coeff2;
+
+                float RHS1 = RHS_Arr[row1];
+                float RHS2 = RHS_Arr[row2];
+                RHSSum = RHS1 - RHS2;
+
+
+                if (coeffSum == 0)
+                    continue;
+                if (!firstTerm && coeffSum > 0) {
+                    cout << "+";
+                }
+                cout << coeffSum << "x" << col + 1;
+                firstTerm = false;
+            }
+            cout << " = " << RHSSum << endl;
+        }
+        // ================================
+        // SUBSTITUTE BETWEEN 2 EQUATIONS
+        // ================================
+        //Substitute X of n in equation No.1 with X of n in equation No.2
+        if (command.substr(0, 10) == "substitute") {
+            string word;
+            string replacement;
+            int repIndex = 0;
+            int eq1;
+            int eq2;
+
+            stringstream sepCommand(command);
+            sepCommand >> word;
+            sepCommand >> replacement;
+            sepCommand >> eq1;
+            sepCommand >> eq2;
+
+            if (eq1 <= 0 || eq2 <= 0 || eq1 > n || eq2 > n) {
+                cout << "No equations available with this index, equations start from 1 to " << n << endl;
+                continue;
+            }
+
+            for (int i = 0; i < replacement.length(); i++) {
+                if (replacement[i] == 'x') continue;
+                repIndex = repIndex * 10 + (replacement[i] - '0');
+            }
+            
+            int row1 = eq1 - 1;
+            int row2 = eq2 - 1;
+            int col = repIndex - 1;
+            //Replace X value from a function and substitute it in every other term with the same value of X
+            //Say 2x1 + 3x2 = 6 --> x2 = (6 - 2x1)/3
+            //Now in another equation 4x1 + 6x2 = 10 --> 4x1 + (6*(6-2x1))/3
+                
+                //if there's no X of n in the source equation
+                if (coefficient[row2][col] == 0) {
+                    cout << "x" << repIndex << " does not exist in equation " << eq2 << endl;
+                    continue;
+                }
+
+                float tempCoeff[100];
+                //Gaussian Elimination 
+                // eq1: a1​x1​+a2​x2​+a3​x3​=b1​
+                // eq2: c1​x1​+c2​x2​+c3​x3​=b2​
+                // x2 from eq2 to use in 1 = b2-c1x1-c3x2/c2
+                //substituting in eq1: a1x1+a2(b2-c1x1-c3x2/c2)+a3x3=b1
+                //normalizing the eqaution: a1x1+(a2/c2)*b2-(a2/c2)*c1x1-(a2/c2)*c3x3+a3x3=b1
+                //Grouping like terms: (a1-(a2/c2)c1)x1 | (a3-(a2/c2)c3)x3 | b1-(a2/c2)b2
+
+                // coeffMultiplier = a2/c2
+                float coeffMultiplier = coefficient[row1][col] / coefficient[row2][col];
+                
+                for (int CoeffInd = 0; CoeffInd < n; CoeffInd++) {
+                  //tempCoeff           =         an                  -     (a2/c2)     *             cn
+                    tempCoeff[CoeffInd] = coefficient[row1][CoeffInd] - coeffMultiplier * coefficient[row2][CoeffInd];
+                }
+                    //newRHS = b1            -     (a2/c2)           b2
+                float newRHS = RHS_Arr[row1] - coeffMultiplier * RHS_Arr[row2];
+
+                bool firstTerm = true;
+
+                for (int cof = 0; cof < n; cof++) 
+                {
+                    //if the resulte of substitution is 0
+                    if (tempCoeff[cof] == 0) continue;
+
+                    if (!firstTerm && tempCoeff[cof] > 0) {
+                        cout << "+";
+                    }
+
+                    cout << tempCoeff[cof] << "x" << cof + 1;
+                    firstTerm = false;
+                }
+                cout << " = " << newRHS << endl;
+        }
+        // ================================
+        // GET DETERMINENTS COEFFICIENT MATRIX
+        // ================================
+        if (command == "D") {
+            cout << "      ";
+            for (int j = 0; j < n; j++) {
+                cout << "   x" << j + 1 << "   ";
+            }
+            cout << endl;
+            cout << "----------------------------------\n";
+           
+            for (int i = 0; i < n; i++) {
+                cout << "Eq" << i + 1 << " | ";
+
+                for (int j = 0; j < n; j++) {
+                    cout.width(6);
+                    cout << coefficient[i][j] << " ";
+                }
+                cout << endl;
+            }
+        }
+
+        // =================================================================
+        // GET DETERMINENTS COEFFICIENT MATRIX BY REPLACTING VARIABLE COLUMN
+        // =================================================================
+        if (command.substr(0,1) == "D" && command != "D_value" && command != "D") {
+            string D;
+            string var;
+            
+            stringstream cramer(command);
+
+            cramer >> D;
+            cramer >> var;
+
+            int varIndex = 0;
+
+            for (int i = 0; i < var.length(); i++)
+            {
+                if (var[i] == 'x') continue;
+                varIndex = varIndex * 10 + (var[i] - '0');
+            }
+
+            int targetCol = varIndex - 1;
+
+            cout << "      ";
+            for (int j = 0; j < n; j++) {
+                cout << "   x" << j + 1 << "   ";
+            }
+            cout << endl;
+            cout << "----------------------------------\n";
+           
+            for (int i = 0; i < n; i++) {
+                cout << "Eq" << i + 1 << " | ";
+
+                for (int j = 0; j < n; j++) {
+                    //if we are at the target column, then print the RHS instead of the xj coefficients
+                    if (j == targetCol) {
+                        cout.width(6);
+                        cout << RHS_Arr[i] << " ";
                     }
                     else {
+                        cout.width(6);
+                        cout << coefficient[i][j] << " ";
+                    }
+                }
+                cout << endl;
+            }
+        }
+        // ================================
+        // Get the Determinant of the Matrix using Gaussian Elimination
+        // ================================
+
+        float det = 1;
+        if (command == "D_value") {
+            //Make a temp compy of the orignal matrix
+            float tempMatrix[100][100];
+            for (int row = 0; row < n; row++) {
+                for (int col = 0; col < n; col++) {
+                    tempMatrix[row][col] = coefficient[row][col];
+                }
+            }
+
+            //use Gaussian Elimination to make the Matrix Diagonal
+            //first loop to iterate n-1 ( The number of columns needed to produce a diagonal matrix )
+            for (int targetCol = 0; targetCol < n-1; targetCol++) {
+
+                //Loop on the rows starting from the second row each time
+                for (int targetRow = targetCol + 1; targetRow < n; targetRow++) {
+
+                    // tempMatrix[targetRow][targetCol] -> gets the target coefficient for the multiplier denominator 
+                    // tempMatrix[targetCol][targetCol] -- > gets the one needed element ( Pivot element in Gauss Eliminations terminology) to eliminate all the values under the same column 
+                    if (tempMatrix[targetCol][targetCol] == 0) {
+                        cout << "Element used from Target column = Zero, Determinent can't be calculated";
                         break;
+                    }
+                    float multiplier = tempMatrix[targetRow][targetCol] / tempMatrix[targetCol][targetCol];
+
+
+                    //after calculating the multiplier, eliminate the lower left triangle in the matrix to make an upper right triangle 
+                    for (int col = targetCol; col < n; col++) {
+                        tempMatrix[targetRow][col] -= multiplier * tempMatrix[targetCol][col];
                     }
                 }
             }
-            cout << equations[eNumber] << "\n";
+            
+            for (int i = 0; i < n; i++) {
+                det *= tempMatrix[i][i];
+            }
+            cout << det << endl;
+        }
+        // ====================================================
+        // Using Gaussian Elimination to Solve the System 
+        // ====================================================
+        if (command == "solve") {
+            float tempMatrix[100][100];
+            for (int row = 0; row < n; row++) {
+                for (int col = 0; col < n; col++) {
+                    tempMatrix[row][col] = coefficient[row][col];
+                }
+            }
+            float tempRHS[100];
+            for (int row = 0; row < n; row++) {
+                for (int col = 0; col < n; col++) {
+                    tempRHS[row] = RHS_Arr[row];
+                }
+            }
+            //first loop to iterate n-1 ( The number of columns needed to produce a diagonal matrix )
+            for (int targetCol = 0; targetCol < n - 1; targetCol++) {
+                // tempMatrix[targetRow][targetCol] -> gets the target coefficient for the multiplier denominator 
+                // tempMatrix[targetCol][targetCol] -- > gets the one needed element ( Pivot element in Gauss Eliminations terminology) to eliminate all the values under the same column 
+                if (tempMatrix[targetCol][targetCol] == 0) {
+                    cout << "Element used from Target column = Zero, Determinent can't be calculated";
+                    break;
+                }
+                //Loop on the rows starting from the second row each time
+                for (int targetRow = targetCol + 1; targetRow < n; targetRow++) {
+
+                    float multiplier = tempMatrix[targetRow][targetCol] / tempMatrix[targetCol][targetCol];
+
+
+                    //after calculating the multiplier, eliminate the lower left triangle in the matrix to make an upper right triangle 
+                    for (int col = targetCol; col < n; col++) {
+                        tempMatrix[targetRow][col] -= multiplier * tempMatrix[targetCol][col];
+                    }
+                }
+            }
         }
 
         if (command == "quit") {
@@ -208,21 +551,17 @@ int main()
     }
 
 
-
-    
     cout << "\nAugmented Matrix:\n\n";
 
-    // Print header row
+    // Print header row representing X of n
     cout << "      ";
     for (int j = 0; j < n; j++) {
         cout << "   x" << j + 1 << "   ";
     }
     cout << " |   RHS\n";
-
-    // Print separator line
     cout << "--------------------------------------------------\n";
 
-    // Print matrix rows
+    // Print matrix rows Eqch equation
     for (int i = 0; i < n; i++) {
         cout << "Eq" << i + 1 << " | ";
 
---
+++ README.md
@@ -0,0 +1,34 @@
+## Linear Equation Solver (C++)
+
+### Description
+
+This program reads a system of linear equations and generates the augmented matrix representation.
+
+Features :
+
+- Parses equations dynamically
+
+- Supports implicit coefficients
+
+- Command interface
+
+- Prints formatted matrix
+
+### Requirements
+
+- C++ Compiler (g++)
+
+- Standard C++11 or higher
+
+- Compile
+  g++ LinearEquationCalc.cpp -o solver
+  Run
+  ./solver
+
+### Commands
+
+- num_vars
+
+- equation i
+
+- quit
